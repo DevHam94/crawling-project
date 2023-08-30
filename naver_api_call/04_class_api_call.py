831338e2a3b9667a7afb8b781b62eaee3ae74765
backwizard0 <gkaeogus7@gmail.com>
2023-08-31 01:20:39 +0900

    20230831

--- naver_api_call/04_class_api_call.py
+++ naver_api_call/04_class_api_call.py
@@ -17,6 +17,11 @@ class NaverSearchApi():
             exit("Error 최대 요청할 수 있는 건수는 1100건 입니다.")
 
         repeat = quantity // 100  # 1000총 10번
+        display = 100
+        if quantity < 100:
+            display = quantity
+            repeat = 1
+
         result = []
         for i in range(repeat):
             start = i * 100 + 1
@@ -24,7 +29,7 @@ class NaverSearchApi():
             if start > 1000:
                 start = 1000
             print(f"{i + 1}번 반복 합니다. start:{start}")
-            r = self.call_api(keyword, start=1, display=100)
+            r = self.call_api(keyword, start=start, display=display)
             print(r['items'][0])
             result += r['items']
         return result
@@ -37,7 +42,11 @@ class NaverSearchApi():
         self.api_url = "https://openapi.naver.com/v1/search/news.json"
         return self.call_api(keyword)
 
+    def webkr(self, keyword, quantity=100):
+        self.api_url = "https://openapi.naver.com/v1/search/webkr.json"
+        return self.get_paging_call(keyword, quantity)
+
 if __name__ == '__main__':
     naver_search_api = NaverSearchApi()
-    r = naver_search_api.news("사당역 족발집", 1000)
-    print(r['items'][0])
+    r = naver_search_api.webkr("사당역 족발집", 1000)
+    print(r[0])
