8fa687c27ba54b665d53a8c2f0944691e29a696c
backwizard0 <gkaeogus7@gmail.com>
2023-08-31 01:15:43 +0900

    20230831

--- naver_api_call/04_class_api_call.py
+++ naver_api_call/04_class_api_call.py
@@ -11,15 +11,33 @@ class NaverSearchApi():
         r = res.json()
         return r
 
-    def blog(self, keyword):
-        self.api_url = "https://openapi.naver.com/v1/search/blog.json"
-        return self.call_api(keyword)
+    def get_paging_call(self, keyword, quantity):
+        if quantity > 1100:
+            # quantity = 1100
+            exit("Error 최대 요청할 수 있는 건수는 1100건 입니다.")
 
-    def news(self, keyword):
+        repeat = quantity // 100  # 1000총 10번
+        result = []
+        for i in range(repeat):
+            start = i * 100 + 1
+            # 101
+            if start > 1000:
+                start = 1000
+            print(f"{i + 1}번 반복 합니다. start:{start}")
+            r = self.call_api(keyword, start=1, display=100)
+            print(r['items'][0])
+            result += r['items']
+        return result
+
+    def blog(self, keyword, quantity=100):
+        self.api_url = "https://openapi.naver.com/v1/search/blog.json"
+        return self.get_paging_call(keyword, quantity)
+
+    def news(self, keyword, quantity=100):
         self.api_url = "https://openapi.naver.com/v1/search/news.json"
         return self.call_api(keyword)
 
 if __name__ == '__main__':
     naver_search_api = NaverSearchApi()
-    r = naver_search_api.news("사당역 족발집")
-    print(r['items'])
+    r = naver_search_api.news("사당역 족발집", 1000)
+    print(r['items'][0])
