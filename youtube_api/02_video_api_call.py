cf7b4c7550262d8aee05241946ba06247cce1ddf
backwizard0 <gkaeogus7@gmail.com>
2023-09-04 01:48:43 +0900

    20230904

--- youtube_api/02_video_api_call.py
+++ youtube_api/02_video_api_call.py
@@ -1,5 +1,6 @@
 from googleapiclient.discovery import build
 import os, json
+import pandas as pd
 
 DEVELOPER_KEY = os.getenv("YOUTUBE_API_KEY")
 
@@ -9,7 +10,7 @@ class YoutubeApi:
 
         self.youtube_api = build("youtube", "v3", developerKey=api_key)
 
-    def youtube_search(self, query: object) -> object:
+    def video_search_list(self, query: object) -> object:
         search_response = self.youtube_api.search().list(
             q=query,
             part="id,snippet",
@@ -44,13 +45,30 @@ class YoutubeApi:
             part='snippet,statistics',
             maxResults=max_cnt
         ).execute()
-
+        comments = []
         for comment in comment_list_response.get("items",):
             snippet = comment['snippet']['topLevelComment']['snippet']
             map = {"videoId":snippet["videoId"], "textOriginal":snippet['textOriginal'], "authorDisplayName":snippet["authorDisplayName"]}
-            print(map)
+            comments.append(map)
             # print(snippet['textOriginal'], snippet['authorDisplayName'])
             # print(json.dumps(comment))
+        return comments
+
+    def save_to_excel(self, search_result, filename):
+        df = pd.DataFrame(search_result)
+        df.to_excel(filename)
+
+    def crawl_comment_by_keyword(self, keyword, video_cnt=5):
+        video_ids = self.video_search_list(keyword, video_cnt)
+        r_video = self.video(video_ids)
+        # print("vids:", r_video)
+        l = []
+        for video in r_video:
+            cnt = video['commentCount']
+            if cnt > 100:
+                cnt = 100
+            comment_list = self.comment(video['video_id'], cnt)
+            l += comment_list
 
 if __name__ == "__main__":
     DEVELOPER_KEY = os.getenv("YOUTUBE_API_KEY")
@@ -62,5 +80,8 @@ if __name__ == "__main__":
     # print(r_video)
     # for item in r_video:
     #     print(item)
-
-    api.comment("", 61)
+    keyword = "핸드크림"
+    l = api.crawl_comment_by_keyword("변호사 vlog")
+    api.save_to_excel(l, f"{keyword}.xlsx")
+    # comment_results = api.comment("", 61)
+    # api.save_to_excel(comment_results, "변호사 vlog.xlsx")
